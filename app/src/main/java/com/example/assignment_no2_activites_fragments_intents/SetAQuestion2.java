7d90d20b6d246cf82e9ce5df97102d804cda98eb
sahil sayyad <igsahilsayyad@gmail.com>
2024-04-02 05:23:24 +0530

    assignment number 2

---
+++ app/src/main/java/com/example/assignment_no2_activites_fragments_intents/SetAQuestion2.java
@@ -0,0 +1,35 @@
+package com.example.assignment_no2_activites_fragments_intents;
+/*
+  SET-A
+       2.Create a simple application which send hello message from one activity to another
+         with help of Button(Use intent).
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class SetAQuestion2 extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_a_question_2);
+
+        Button sendButton = findViewById(R.id.button_send);
+
+        sendButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                sendMessage();
+            }
+        });
+
+    }
+    private void sendMessage(){
+        Intent intent = new Intent(this,SetAQuestion2B.class);
+        intent.putExtra("message", "Hello Sahil");
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/assignment_no2_activites_fragments_intents/SetAQuestion2B.java
@@ -0,0 +1,27 @@
+package com.example.assignment_no2_activites_fragments_intents;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class SetAQuestion2B extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_a_question_2b);
+
+        TextView messageTextView = findViewById(R.id.text_message);
+
+        Intent intent = getIntent();
+
+        if(intent != null && intent.hasExtra("message")){
+            String message = intent.getStringExtra("message");
+            messageTextView.setText(message);
+        }else{
+            messageTextView.setText("No Message received");
+        }
+
+    }
+}
