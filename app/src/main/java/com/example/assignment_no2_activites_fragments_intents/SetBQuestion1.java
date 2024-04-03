2b76613d12d6800f94aded0f1278edbc1a9db915
sahil sayyad <igsahilsayyad@gmail.com>
2024-04-04 01:19:18 +0530

    assignment number 2 set b question one done

---
+++ app/src/main/java/com/example/assignment_no2_activites_fragments_intents/SetBQuestion1.java
@@ -0,0 +1,49 @@
+package com.example.assignment_no2_activites_fragments_intents;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+public class SetBQuestion1 extends AppCompatActivity{
+
+    private static final String USERNAME="user";
+    private static final String PASSWORD="password";
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_b_question_1);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                 login();
+            }
+        });
+
+    }
+
+    private void login(){
+        String username= editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+        System.out.println(username);
+
+        if(username.equals(USERNAME) && password.equals(PASSWORD)){
+            Intent intent = new Intent(this,SetBQuestion1B.class);
+            startActivity(intent);
+            finish();
+            Toast.makeText(this,"Login Success",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"Invalid username or password",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/assignment_no2_activites_fragments_intents/SetBQuestion1B.java
@@ -0,0 +1,12 @@
+package com.example.assignment_no2_activites_fragments_intents;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class SetBQuestion1B extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_b_question_1b);
+
+    }
+}
