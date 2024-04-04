f4916da3fc540c1bb3966700e5b947a591081d7a
sahil sayyad <igsahilsayyad@gmail.com>
2024-04-04 14:06:53 +0530

    assignment number 3 set a question number 1 and 2 done

---
+++ Assignment_No_3_Android_User_Interface/app/src/main/java/com/example/assignment_no_3_android_user_interface/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.assignment_no_3_android_user_interface;
+/* SET A
+       1.Design Following
+                 - add a border to an Android Layout.
+
+       2.Design an Android Portrait and Landscape Screen Layout Example.
+ */
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        }
+    }
---
+++ Assignment_No_3_Android_User_Interface/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Assignment_No_3_Android_User_Interface"
+include(":app")
+ 
