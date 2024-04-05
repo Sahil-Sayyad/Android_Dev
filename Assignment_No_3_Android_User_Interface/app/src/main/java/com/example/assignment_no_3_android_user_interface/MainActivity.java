a4b28f217b240657625a6a4b465100090c879679
sahil sayyad <igsahilsayyad@gmail.com>
2024-04-06 01:48:18 +0530

    assignment number 3 set b question number 2 done

--- Assignment_No_3_Android_User_Interface/app/src/main/java/com/example/assignment_no_3_android_user_interface/MainActivity.java
+++ Assignment_No_3_Android_User_Interface/app/src/main/java/com/example/assignment_no_3_android_user_interface/MainActivity.java
@@ -4,6 +4,9 @@ package com.example.assignment_no_3_android_user_interface;
                  - add a border to an Android Layout.
 
        2.Design an Android Portrait and Landscape Screen Layout Example.
+
+  SET B
+       2.create following Vertical and Horizontal Scroll View Creation in Android.
  */
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+       // setContentView(R.layout.activity_main);
+        setContentView(R.layout.set_b_question_2);
+       // setContentView(R.layout.set_b_question_2b);
         }
     }
